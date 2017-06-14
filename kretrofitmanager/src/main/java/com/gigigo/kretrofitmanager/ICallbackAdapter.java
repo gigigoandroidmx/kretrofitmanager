76e46705405bead780d3aaaca74e5108e41aec8a
Juan Godinez Vera <juan.godinez@gigigo.com.mx>
2017-06-14 17:55:58 -0500

    Se actualiza el proyecto a librería

---
+++ kretrofitmanager/src/main/java/com/gigigo/kretrofitmanager/ICallbackAdapter.java
@@ -0,0 +1,14 @@
+package com.gigigo.kretrofitmanager;
+
+import retrofit2.Response;
+
+/**
+ * @author Juan Godínez Vera - 5/10/2017.
+ */
+public interface ICallbackAdapter<T> {
+    void onDataEmpty();
+    void onSuccess(T data);
+    void onUnauthorized();
+    void onError(Throwable exception);
+    void onDataNotAvailable(ResponseState entryState);
+}
